ecc31cb6682f79ba625da9f118853d813d7955d5
Bruno Avelar <brunoavelar@Brunos-Air.domain>
2016-06-14 15:39:07 -0400

    Mock api and CoursesList

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+store.dispatch(loadCourses());
+
 render(
-	<Router history={browserHistory} routes={routes} />,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</Provider>,
 	document.getElementById('app')
 );
